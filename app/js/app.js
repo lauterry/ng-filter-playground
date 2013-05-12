60a0f9059d145cfac08d4cf756449549f9fc47bf
Thierry LAU <thierlau@gmail.com>
2013-05-12 19:56:08 +0200

    remove extra

--- app/js/app.js
+++ app/js/app.js
@@ -4,19 +4,11 @@ var angularMovieApp = angular.module('angularMovieApp', []);
 
 angularMovieApp.config(function($routeProvider) {
     $routeProvider
-        .when('/home', {
-            templateUrl: 'partials/home.html',
-            controller : 'homeController'
-        })
         .when('/movies', {
             templateUrl: 'partials/movies.html',
             controller : 'moviesController'
         })
-        .when('/movies/edit/:id', {
-            templateUrl: 'partials/edit.html',
-            controller: 'editMovieController'
-        })
         .otherwise({
-            redirectTo: '/home'
+            redirectTo: '/movies'
         });
 });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,47 +12,4 @@ angularMovieApp.controller("moviesController" ,function ($scope, Movie) {
         $scope.movies = resp.movies;
     });
 
-
-    $scope.deleteMovie = function(index){
-        Movie.remove($scope.movies[index].id)
-            .success(function(resp){
-                $scope.movies.splice(index, 1);
-            }
-        );
-    };
-
-});
-
-angularMovieApp.controller('editMovieController', function($scope, Movie, $routeParams, $location){
-
-    var movieId = $routeParams.id;
-
-    Movie.fetchOne(movieId).success(function(movie){
-       $scope.movie = movie;
-    });
-
-    $scope.updateMovie = function(movie){
-       Movie.update(movie)
-           .success(function(){
-               $location.path('/movies');
-           })
-           .error(function(resp){
-               console.log(resp);
-           });
-    };
-});
-
-
-angularMovieApp.controller("movieFormController" ,function ($scope, Movie) {
-    $scope.addMovie = function(movie){
-
-        Movie.create(movie)
-            .success(function(){
-                $scope.movies.push(movie);
-                $scope.movie = {};
-            })
-            .error(function(resp){
-                console.log(resp);
-            });
-    };
 });
