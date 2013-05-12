1faef3c1a2df6f489cdf5e7219afc6baf4f99cce
Thierry LAU <thierlau@gmail.com>
2013-05-12 12:14:07 +0200

    initial commit

---
+++ app/js/controllers.js
@@ -0,0 +1,58 @@
+"use strict";
+
+angularMovieApp.controller("homeController" ,function ($scope) {
+
+    $scope.user = 'Thierry LAU';
+
+});
+
+angularMovieApp.controller("moviesController" ,function ($scope, Movie) {
+
+    Movie.fetch().success(function(resp){
+        $scope.movies = resp.movies;
+    });
+
+
+    $scope.deleteMovie = function(index){
+        Movie.remove($scope.movies[index].id)
+            .success(function(resp){
+                $scope.movies.splice(index, 1);
+            }
+        );
+    };
+
+});
+
+angularMovieApp.controller('editMovieController', function($scope, Movie, $routeParams, $location){
+
+    var movieId = $routeParams.id;
+
+    Movie.fetchOne(movieId).success(function(movie){
+       $scope.movie = movie;
+    });
+
+    $scope.updateMovie = function(movie){
+       Movie.update(movie)
+           .success(function(){
+               $location.path('/movies');
+           })
+           .error(function(resp){
+               console.log(resp);
+           });
+    };
+});
+
+
+angularMovieApp.controller("movieFormController" ,function ($scope, Movie) {
+    $scope.addMovie = function(movie){
+
+        Movie.create(movie)
+            .success(function(){
+                $scope.movies.push(movie);
+                $scope.movie = {};
+            })
+            .error(function(resp){
+                console.log(resp);
+            });
+    };
+});
