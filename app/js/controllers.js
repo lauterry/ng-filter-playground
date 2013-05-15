c6dd3002870d6fba351f8e0ae743423e40d14976
Thierry LAU <thierlau@gmail.com>
2013-05-16 00:06:01 +0200

    remove homecontroller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,5 @@
 "use strict";
 
-angularMovieApp.controller("homeController" ,function ($scope) {
-
-    $scope.user = 'Thierry LAU';
-
-});
-
 angularMovieApp.controller("moviesController" ,function ($scope, Movie) {
 
     Movie.fetch().success(function(resp){
